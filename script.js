dee2d7ce9705867312791824d1f62f0f030b81f2
simba <simba.fs@gmail.com>
2019-08-20 22:07:22 +0800

    do someting

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
+// alias 
+const log = console.log;
+
 var tet = $('#tet');
+
+function right(){
+	log('right');
+}
+
+function left(){
+	log('left');
+}
+
+function down(){
+	log('down');
+}
+
+function flipR(){
+	log('flipR');
+}
+
+function flipL(){
+	log('flipL');
+}
+
+function flipU(){
+	log('flipU');
+}
+
+// bind keypress
 var config = {
 	right:{
 		key: 'right',
@@ -19,25 +48,15 @@ var config = {
 	flipL: {
 		key: 'd',
 		fn: flipL
+	},
+	flipU: {
+		key: 's',
+		fn: flipU
 	}
 }
 
-function right(){
 
-}
-
-function left(){
-
-}
-
-function down(){
-
-}
-
-function flipR(){
-
-}
-
-function flipL(){
-
-}
+$document = $(document);
+$.each(config, (index, item) => {
+	$document.bind('keydown', item.key, item.fn);
+});
